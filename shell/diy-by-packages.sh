0d22c3193cc29504d9b55c1f12d6657b4790de8e
chenui <chenui@users.noreply.github.com>
2022-11-27 14:09:23 +0800

    add pasw repo

--- shell/diy-by-packages.sh
+++ shell/diy-by-packages.sh
@@ -2,6 +2,8 @@
 
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> ./feed.conf.default
+echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> ./feed.conf.default
 git pull
 
 ./scripts/feeds update -a
