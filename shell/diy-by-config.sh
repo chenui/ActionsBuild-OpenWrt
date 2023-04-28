f20b6bce43a56d52c6f0250cfe7678c8b91a23cc
chenui <chenui@users.noreply.github.com>
2023-04-28 08:35:34 +0800

    change ip to 192.168.1.253

--- shell/diy-by-config.sh
+++ shell/diy-by-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #1. 默认ip
-sed -i 's/192.168.1.1/192.168.1.249/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.253/g' openwrt/package/base-files/files/bin/config_generate
